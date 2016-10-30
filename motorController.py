31320002278e67b0f41654bda40414de5708201f
TristanDebrunner <tpwd@me.com>
2016-10-29 21:35:52 -0600

    created motorController.py

---
+++ motorController.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 29, 2016
+
+@author: Tristan
+'''
+
+class MotorController():
+    '''
+    controls the slide motor, including acceleration limits
+    '''
+
+
+    def __init__(self, pi):
+        '''
+        Constructor
+        '''
+        
